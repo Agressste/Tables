8b882bdd72013537d9a8dbe2aaf044260bb43eaf
Гость <Guest@iMac-Mac005-36.local>
2022-04-14 14:01:59 +0900

    first commit

---
+++ Tables/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tables
+//
+//  Created by Гость on 14.04.2022.
+//
+
+import UIKit
+
+@IBOutler weak var cellLabel
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
